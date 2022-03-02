8c170599b1a8f576bd991fd26bab3a8345581a26
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-02 19:23:52 +0300

    Add multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,10 @@ var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
 var result = subtract(number1, number2);
 alert(result);  
+var multiply = function(number1, number2) {
+    return number1 * number2;
+};
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
