c64b75efc8bc54f52a94da328be304867b18bcee
DruSadeMumba <drusademumba@gmail.com>
2022-04-16 18:19:50 +0300

    Modify js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,28 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-};
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-var result = add(number1, number2);
-alert(result); 
-var subtract = function(number1, number2) {
-    return number1 - number2;
-};
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-var result = subtract(number1, number2);
-alert(result);  
-var multiply = function(number1, number2) {
-    return number1 * number2;
-};
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-var divide = function(number1, number2) {
-    return number1 / number2;
-};
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-var result = divide(number1, number2);
-alert(result);
-var bmi = function(weight, height) {
-    return weight / height * height;
-};
-var weight = parseInt(prompt("enter your weight in kgs"));
-var height = parseFloat(prompt("enter your height in meters"));
-var result1 = multiply(height, height);
-var result = divide(weight, result1);
-alert(result);
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+  button.addEventListener('click', (operator) => {
+    switch(operator.target.innerText){
+      case 'C':
+        display.innerText = '';
+          break;
+      case '=':
+        try{
+          display.innerText = eval(display.innerText);
+            } catch {
+              display.innerText = "Error"
+            }
+              break;
+      case '←':
+        if (display.innerText){
+          display.innerText = display.innerText.slice(0, -1);
+            }
+              break;
+      default:
+        display.innerText += operator.target.innerText;
+     }
+  });
+});
+
